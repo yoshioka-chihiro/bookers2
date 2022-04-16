b8372c30783c8e9526ee8e526607c4ddec3cb6ff
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-16 11:00:06 +0000

    [Add]CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,28 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @book = Book.new
-
   end
 
   def show
     @user = User.find(params[:id])
-    @book = @user.books
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction,:profile_image)
+  end
+
 
 end
